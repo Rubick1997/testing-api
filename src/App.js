dd8152ec1b2909874593a9e965b419ac9a10a3b0
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro.local>
2021-05-18 17:04:45 -0700

    fixed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
       );
       setJoke(data);
       setIsLoading(false);
-      console.log(joke)
     };
     fetchJoke();
   }, []);
--- src/App.test.js
+++ src/App.test.js
@@ -2,18 +2,17 @@ import React from "react";
 import { act, render, screen } from "@testing-library/react";
 import App from "./App";
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve({
-        icon_url: "http://image.png",
-        value: "Joke Test",
-      }),
-  })
-);
-
 describe("App", () => {
   it("loads the joke on mount", async () => {
+    global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          icon_url: "http://image.png",
+          value: "Joke Test",
+        }),
+    })
+  );
     await act(async () => render(<App />));
     expect(screen.getByText("Joke Test")).toBeInTheDocument();
   });
